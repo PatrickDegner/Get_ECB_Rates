c96667b8f969b788849c917bb4b4b56fb03be38f
Patrick.Degner <Acaer@gmx.net>
2022-07-02 22:23:05 +0200

    Update response

--- utils/scrapper.py
+++ utils/scrapper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html'
 response = requests.get(url)
-simple_soup = BeautifulSoup(response.text, 'html.parser')
+simple_soup = BeautifulSoup(response.content, 'html.parser')
 
 date = simple_soup.find('h3').string
 country = simple_soup.find_all('td', {'class':'currency'})
